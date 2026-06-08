a315bdd6c0218e068e841a68924bb8f3e1ec599c
Galileu Meirelles <galileu15@gmail.com>
2026-06-08 17:25:44 -0300

    Fix: Galilei atualizado

--- main.py
+++ main.py
@@ -172,14 +172,17 @@ class MenuSimulado(View):
     def __init__(self):
         super().__init__(timeout=None)
 
-    @discord.ui.button(label="Teoria dos Sistemas", style=discord.ButtonStyle.secondary, row=2)
-    async def btn1(self, it, btn): await self.preparar_sala(it, "Teoria dos Sistemas.txt")
+    @discord.ui.button(label="Teoria Geral dos Sistemas", style=discord.ButtonStyle.secondary, row=2)
+    async def btn1(self, it, btn): await self.preparar_sala(it, "Teoria Geral dos Sistemas.txt")
 
     @discord.ui.button(label="Sistemas Operacionais", style=discord.ButtonStyle.secondary, row=2)
     async def btn2(self, it, btn): await self.preparar_sala(it, "Sistemas Operacionais.txt")
 
-    @discord.ui.button(label="Fundamentos de Gestão Empresarial", style=discord.ButtonStyle.secondary, row=2)
-    async def btn3(self, it, btn): await self.preparar_sala(it, "Fundamentos de Gestão Empresarial.txt")
+    @discord.ui.button(label="Introdução à Ciência de Dados", style=discord.ButtonStyle.secondary, row=2)
+    async def btn3(self, it, btn): await self.preparar_sala(it, "Introdução à Ciência de Dados.txt")
+
+    @discord.ui.button(label="Arquitetura de Computadores", style=discord.ButtonStyle.secondary, row=2)
+    async def btn4(self, it, btn): await self.preparar_sala(it, "Arquitetura de Computadores.txt")
 
     async def preparar_sala(self, interaction, nome_arquivo):
         # 1. Cria a thread primeiro
@@ -279,11 +282,10 @@ async def menu(ctx):
         description=(
             "Aqui estão as provas disponíveis neste servidor.\n"
             "Você pode iniciar um simulado clicando no botão correspondente abaixo.\n\n"
-            "**Probabilidade e Estatística**\n"
-            "📌 Vinculado por: @Galileu Meirelles\n\n"
-            "**Fundamentos de Sistemas de Informação**\n"
-            "📌 Vinculado por: @Galileu Meirelles\n\n"
-            "**Fundamentos de Gestão Empresarial**\n"
+            "**Teoria Geral dos Sistemas**\n"
+            "**Sistemas Operacionais**\n"
+            "**Arquitetura de Computadores**\n"
+            "**Introdução à Ciência de Dados**\n"
             "📌 Vinculado por: @Galileu Meirelles\n\n"
             "🔹 *Clique em um dos botões abaixo para abrir sua sala privada!*"
         ),
